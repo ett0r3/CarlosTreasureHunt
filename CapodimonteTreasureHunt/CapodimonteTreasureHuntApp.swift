1271988c2934ca9a77820a549ee0f37579f53e60
ett0r3 <94621596+ett0r3@users.noreply.github.com>
2026-05-29 15:47:46 +0200

    Initial Commit

---
+++ CapodimonteTreasureHunt/CapodimonteTreasureHuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapodimonteTreasureHuntApp.swift
+//  CapodimonteTreasureHunt
+//
+//  Created by AFP FED 003 on 29/05/26.
+//
+
+import SwiftUI
+
+@main
+struct CapodimonteTreasureHuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
