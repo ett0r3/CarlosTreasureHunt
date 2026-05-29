164ab23551bb6615a05fbe68074b614caeddc39a
chiarAI0011 <mariachiaracolonna00@gmail.com>
2026-05-29 15:58:23 +0200

    Home screen Created home screen UI and navigation

--- CapodimonteTreasureHunt/ContentView.swift
+++ CapodimonteTreasureHunt/ContentView.swift
@@ -8,14 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+        NavigationStack {
+
+            ZStack {
+
+                LinearGradient(
+                    colors: [
+
+                        Color(
+                            red: 242 / 255,
+                            green: 234 / 255,
+                            blue: 249 / 255
+                        ),
+
+                        Color(
+                            red: 206 / 255,
+                            green: 189 / 255,
+                            blue: 239 / 255
+                        )
+                    ],
+
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+                .ignoresSafeArea()
+
+
+                Image("home")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(
+                        maxWidth: .infinity,
+                        maxHeight: .infinity
+                    )
+                    .clipped()
+                    .ignoresSafeArea()
+
+
+                NavigationLink(destination: SecondView()) {
+
+                    ZStack {
+
+                        Image("lens")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 350)
+                            .offset(x: 13, y: 60)
+
+                        Text("GIOCA")
+                            .font(.system(size: 42))
+                            .bold()
+                            .foregroundStyle(Color.white)
+                            .offset(x: -10, y: -20)
+                        
+                    }// END ZStack
+                }
+            }// END ZStack
         }
-        .padding()
     }
 }
 
---
+++ CapodimonteTreasureHunt/SecondView.swift
@@ -0,0 +1,27 @@
+//
+//  SecondView.swift
+//  CapodimonteTreasureHunt
+//
+//  Created by AFP FED 02 on 02/06/26.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+
+    var body: some View {
+
+        ZStack {
+
+            Color.white
+                .ignoresSafeArea()
+
+            Text("Second View")
+                .font(.largeTitle)
+        }
+    }
+}
+
+#Preview {
+    SecondView()
+}
